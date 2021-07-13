05c30c597bf0944d050b38c09bff4ebcaaf7f977
Dave Tang <davetingpongtang@gmail.com>
2021-07-13 12:50:22 +0900

    Include padding option

--- script/gtf_to_bed.pl
+++ script/gtf_to_bed.pl
@@ -8,7 +8,7 @@ use strict;
 use Getopt::Std;
 
 my %opts = ();
-getopts('i:f:h:', \%opts);
+getopts('i:f:h:u:d:', \%opts);
 
 if ($opts{'h'} ||
     !exists $opts{'f'} ||
@@ -19,6 +19,14 @@ if ($opts{'h'} ||
 
 my $gtf = $opts{'i'};
 my $my_feature = $opts{'f'};
+my $up = 0;
+my $down = 0;
+if (exists $opts{'u'}){
+   $up = $opts{'u'};
+}
+if (exists $opts{'d'}){
+   $up = $opts{'d'};
+}
 
 if ($gtf =~ /\.gz$/){
    open(IN, '-|', "gunzip -c $gtf") || die "Could not open $gtf: $!\n";
@@ -39,7 +47,23 @@ while(<IN>){
 
    # BED is 0-based
    $start -= 1;
+
+   if ($strand eq '+'){
+      $start = $start - $up;
+      $end = $end + $down;
+      if ($start < 0){
+         $start = 0;
+      }
+   } elsif ($strand eq '-'){
+      $start = $start - $down;
+      $end = $end + $up;
+      if ($start < 0){
+         $start = 0;
+      }
+   }
+
    print join("\t", $sequence, $start, $end, $name, $score, $strand), "\n";
+
 }
 close(IN);
 
@@ -49,6 +73,8 @@ Usage: $0 -f FILE -l STRING
 
 Where:   -i         GTF file
          -f         Feature to keep, e.g. gene, transcript, CDS, exon
+         -u         Add upstream padding, default = 0
+         -d         Add downstream padding, default = 0
          -h         this helpful usage message
 
 EOF
